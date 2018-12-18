db2c8314389355769b67e627c2bee0dd4ae94e73
Phung Lai <tl353@njit.edu>
2018-12-18 06:42:11 -0500

    Edit Answer, Question, and Vote

--- app/Vote.php
+++ app/Vote.php
@@ -20,4 +20,43 @@ class Vote extends Model
     public function voteable(){
         return $this->morphTo();
     }
+
+    public function scopeActive($query)
+    {
+        return $query->where('type', 1);
+    }
+
+    public function votesAllowed(){
+        return (bool) true;
+    }
+
+    public function upVotes(){
+        return $this->votes->where('type', 'up');
+    }
+
+    public function downVotes(){
+        return $this->votes->where('type', 'down');
+    }
+
+    public function getVote(User $user){
+        return $this->votes()->where('user_id', $user->id);
+    }
+
+    public function getVotePoints()
+    {
+        $points = 0;
+        foreach ($this->votes AS $vote) {
+            if($vote->type == 'up')
+                $points = $points + 1;
+            elseif($vote->type == 'down')
+                $points = $points - 1;
+        }
+        return $points;
+    }
+
+    public function checkVoted(User $user)
+    {
+        return $this->votes()->where('user_id', $user->id)->exists();
+    }
+
 }
